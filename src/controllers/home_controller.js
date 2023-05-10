e8635ee322065d6f477bace9749149159217d625
huyhoangminh <huyhoangminh@gmail.com>
2023-05-10 08:00:58 +0700

    main

--- src/controllers/home_controller.js
+++ src/controllers/home_controller.js
@@ -4,7 +4,11 @@ let getHomePage = (req, res) => {
 let getAboutPage = (req, res) => {
   return res.render("component/about.ejs");
 };
+let getCRUD = (req, res) => {
+  return res.send("Get CRUD");
+};
 module.exports = {
   getHomePage,
   getAboutPage,
+  getCRUD,
 };
